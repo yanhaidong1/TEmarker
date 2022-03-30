be57f97d66f5b582743da86740309575563ac7b4
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2022-03-30 16:08:03 -0400

    Add files via upload

--- Download_test_data.py
+++ Download_test_data.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+##updating 033022 we will use the cyverse to download data
 ##this script will download the testing data for step 1,2,3
 ##Also download the testing data for step 0
 
@@ -29,28 +30,35 @@ def get_parsed_args():
 
 
 ##define a function to download testing data
-def download_data_step123 (store_download_dir,name,google_drive_path):
+def download_data_step123 (store_download_dir,name,cyverse_path):
 
-    cmd = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate " + \
-          "\'https://docs.google.com/uc?export=download&id=" + google_drive_path + "\' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=" + google_drive_path + "\" -O " + \
-          store_download_dir + "/" + name + ".zip " + "&& rm -rf /tmp/cookies.txt"
-    subprocess.call(cmd, shell=True)
-
-    cmd = 'unzip ' +  store_download_dir + '/' + name + '.zip -d ' + store_download_dir + '/'
+    cmd = 'wget ' + cyverse_path + ' -O ' + store_download_dir + '/' + name + '.zip'
     subprocess.call(cmd,shell=True)
 
-    cmd = 'rm ' + store_download_dir + '/' + name + '.zip'
+    #cmd = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate " + \
+    #      "\'https://docs.google.com/uc?export=download&id=" + google_drive_path + "\' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=" + google_drive_path + "\" -O " + \
+    #      store_download_dir + "/" + name + ".zip " + "&& rm -rf /tmp/cookies.txt"
+    #subprocess.call(cmd, shell=True)
+
+    #cmd = 'unzip ' +  store_download_dir + '/' + name + '.zip -d ' + store_download_dir + '/'
+    #subprocess.call(cmd,shell=True)
+
+    #cmd = 'rm ' + store_download_dir + '/' + name + '.zip'
+    #subprocess.call(cmd,shell=True)
+
+    #cmd = 'rm -rf ' + store_download_dir + '/__MACOSX' + ' ' + store_download_dir + '/' + name + '/Icon'
+    #subprocess.call(cmd,shell=True)
+
+def download_data_step0 (store_download_dir,name,cyverse_path):
+
+    #cmd = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate " + \
+    #      "\'https://docs.google.com/uc?export=download&id=" + google_drive_path + "\' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=" + google_drive_path + "\" -O " + \
+    #      store_download_dir + "/" + name + ".tar.gz " + "&& rm -rf /tmp/cookies.txt"
+    #subprocess.call(cmd, shell=True)
+
+    cmd = 'wget ' + cyverse_path + ' -O ' + store_download_dir + '/' + name + '.zip'
     subprocess.call(cmd,shell=True)
 
-    cmd = 'rm -rf ' + store_download_dir + '/__MACOSX' + ' ' + store_download_dir + '/' + name + '/Icon'
-    subprocess.call(cmd,shell=True)
-
-def download_data_step0 (store_download_dir,name,google_drive_path):
-
-    cmd = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate " + \
-          "\'https://docs.google.com/uc?export=download&id=" + google_drive_path + "\' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id=" + google_drive_path + "\" -O " + \
-          store_download_dir + "/" + name + ".tar.gz " + "&& rm -rf /tmp/cookies.txt"
-    subprocess.call(cmd, shell=True)
 
 
 def main(argv=None):
@@ -66,12 +74,12 @@ def main(argv=None):
 
     ##if users want to download testing data for Step 1,2,3
     if args.s123_yes is not None:
-        google_drive_path = '1N9hb1WFWdzrP0kQ4v_uxaH8VfeOjZS2n'
-        download_data_step123(output_dir,'Example_dir_step123',google_drive_path)
+        cyverse_path = 'https://data.cyverse.org/dav-anon/iplant/home/yanhaidong1991/Example_dir_step123.zip'
+        download_data_step123(output_dir,'Example_dir_step123',cyverse_path)
 
     if args.s0_yes is not None:
-        google_drive_path = '1TwaDBGYQ2OoHIENUgGMfQF8eiO3T1aZV'
-        download_data_step0(output_dir, 'Example_dir_step0', google_drive_path)
+        cyverse_path = 'https://data.cyverse.org/dav-anon/iplant/home/yanhaidong1991/Example_dir_step0.tar.gz'
+        download_data_step0(output_dir, 'Example_dir_step0', cyverse_path)
 
 
 if __name__ == "__main__":
